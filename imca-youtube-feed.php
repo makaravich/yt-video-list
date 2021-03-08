246f58ebbcdea80e9d9c11a82a218ddc9508cf8c
makaravich <d.makarski@gmail.com>
2021-03-09 00:03:13 +0300

    V 1.0.4 small fixes 3

--- imca-youtube-feed.php
+++ imca-youtube-feed.php
@@ -78,7 +78,7 @@ if (is_admin()) { // note the use of is_admin() to double check that this is hap
 		'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
 		'proper_folder_name' => 'yt-video-list', // this is the name of the folder your plugin lives in
 		'api_url' => 'https://api.github.com/repos/makaravich/yt-video-list', // the GitHub API url of your GitHub repo
-		'raw_url' => 'https://raw.github.com/makaravich/yt-video-list/main', // the GitHub raw url of your GitHub repo
+		'raw_url' => 'https://raw.githubusercontent.com/makaravich/yt-video-list/main', // the GitHub raw url of your GitHub repo
 		'github_url' => 'https://github.com/makaravich/yt-video-list', // the GitHub url of your GitHub repo
 		'zip_url' => 'https://github.com/makaravich/yt-video-list/zipball/main', // the zip url of the GitHub repo
 		'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
