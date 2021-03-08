d81d50d310f952d0a4592c3f38b5b6499afa8c56
makaravich <d.makarski@gmail.com>
2021-03-09 00:07:13 +0300

    V 1.0.5 release

--- imca-youtube-feed.php
+++ imca-youtube-feed.php
@@ -81,7 +81,7 @@ if (is_admin()) { // note the use of is_admin() to double check that this is hap
 		'raw_url' => 'https://raw.githubusercontent.com/makaravich/yt-video-list/main', // the GitHub raw url of your GitHub repo
 		'github_url' => 'https://github.com/makaravich/yt-video-list', // the GitHub url of your GitHub repo
 		'zip_url' => 'https://github.com/makaravich/yt-video-list/zipball/main', // the zip url of the GitHub repo
-		'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+		'sslverify' => false, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
 		'requires' => '3.0', // which version of WordPress does your plugin require?
 		'tested' => '3.3', // which version of WordPress is your plugin tested up to?
 		'readme' => 'README.md', // which file to use as the readme for the version number
